8e3068de8ad8c406c6909fa86ed3696178edab48
Timam Bin Saif Tahmid <55949364+TimamBinSaifTahmid@users.noreply.github.com>
2022-01-11 14:11:36 +0600

    array problem leetcode

---
+++ Array-Data-Structure/Basic_Array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     vector<int > result;
+     vector<int > nums={1,2,3,4,0};
+
+        int size=nums.size();
+        for(int i=0;i<size;i++){
+            result.push_back(nums[nums[i]]);
+            cout<<nums[nums[i]]<<endl;
+        }
+
+}
